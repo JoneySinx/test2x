ed2d25069d3efff3babd6a38933c07e62216a24c
𝕊ℙ𝕐𝔻𝔼ℝ <162447832+JoneySinx@users.noreply.github.com>
2025-12-16 23:09:03 +0530

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,8 +1,6 @@
-# Credit - adarsh-goel
-
 from aiohttp import web
-from web.stream_routes import routes
+from web.route import routes
 
-
-web_app = web.Application()
+# client_max_size added to prevent 413 Payload Too Large errors
+web_app = web.Application(client_max_size=30000000)
 web_app.add_routes(routes)
