c23bdc09a84b8ac507c93bf8eeda6a83f00a6b76
𝕊ℙ𝕐𝔻𝔼ℝ <162447832+JoneySinx@users.noreply.github.com>
2025-12-16 22:41:32 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -4,11 +4,15 @@ from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video
 
-
 @Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
 async def media(bot, message):
     """Media Handler"""
+    # मीडिया ऑब्जेक्ट निकालें (Video या Document)
     media = getattr(message, message.media.value, None)
-    if (str(media.file_name).lower()).endswith(tuple(INDEX_EXTENSIONS)):
-        media.caption = message.caption
-        await save_file(media)
+    
+    # चेक करें कि मीडिया और फाइल का नाम मौजूद है या नहीं
+    if media and media.file_name:
+        if str(media.file_name).lower().endswith(tuple(INDEX_EXTENSIONS)):
+            # save_file फंक्शन के लिए कैप्शन को मीडिया ऑब्जेक्ट में जोड़ें
+            media.caption = message.caption
+            await save_file(media)
