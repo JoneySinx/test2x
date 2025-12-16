9ed7839b1973b1efe75b6e001c424e4821743a4f
𝕊ℙ𝕐𝔻𝔼ℝ <162447832+JoneySinx@users.noreply.github.com>
2025-12-16 22:40:01 +0530

    Update group_management.py

--- plugins/group_management.py
+++ plugins/group_management.py
@@ -21,6 +21,7 @@ async def members_management(client, message):
 async def ban_chat_user(client, message):
     if not await is_check_admin(client, message.chat.id, message.from_user.id):
         return await message.reply_text('You not admin in this group.')
+        
     if message.reply_to_message and message.reply_to_message.from_user:
         user_id = message.reply_to_message.from_user.username or message.reply_to_message.from_user.id
     else:
@@ -28,18 +29,23 @@ async def ban_chat_user(client, message):
             user_id = message.text.split(" ", 1)[1]
         except IndexError:
             return await message.reply_text("Reply to any user message or give user id, username")
+            
     try:
         user_id = int(user_id)
     except ValueError:
         pass
+        
     try:
         user = (await client.get_chat_member(message.chat.id, user_id)).user
     except:
         return await message.reply_text("Can't find you given user in this group")
+        
     try:
-        await client.ban_chat_member(message.from_user.id, user_id)
-    except:
-        return await message.reply_text("I don't have access to ban user")
+        # MISTAKE FIXED: message.from_user.id ki jagah message.chat.id lagaya hai
+        await client.ban_chat_member(message.chat.id, user_id)
+    except Exception as e:
+        return await message.reply_text(f"I don't have access to ban user. Error: {e}")
+        
     await message.reply_text(f'Successfully banned {user.mention} from {message.chat.title}')
 
 
@@ -47,6 +53,7 @@ async def ban_chat_user(client, message):
 async def mute_chat_user(client, message):
     if not await is_check_admin(client, message.chat.id, message.from_user.id):
         return await message.reply_text('You not admin in this group.')
+        
     if message.reply_to_message and message.reply_to_message.from_user:
         user_id = message.reply_to_message.from_user.username or message.reply_to_message.from_user.id
     else:
@@ -54,18 +61,22 @@ async def mute_chat_user(client, message):
             user_id = message.text.split(" ", 1)[1]
         except IndexError:
             return await message.reply_text("Reply to any user message or give user id, username")
+            
     try:
         user_id = int(user_id)
     except ValueError:
         pass
+        
     try:
         user = (await client.get_chat_member(message.chat.id, user_id)).user
     except:
         return await message.reply_text("Can't find you given user in this group")
+        
     try:
         await client.restrict_chat_member(message.chat.id, user_id, ChatPermissions())
     except:
         return await message.reply_text("I don't have access to mute user")
+        
     await message.reply_text(f'Successfully muted {user.mention} from {message.chat.title}')
 
 
@@ -73,6 +84,7 @@ async def mute_chat_user(client, message):
 async def unban_chat_user(client, message):
     if not await is_check_admin(client, message.chat.id, message.from_user.id):
         return await message.reply_text('You not admin in this group.')
+        
     if message.reply_to_message and message.reply_to_message.from_user:
         user_id = message.reply_to_message.from_user.username or message.reply_to_message.from_user.id
     else:
@@ -80,16 +92,20 @@ async def unban_chat_user(client, message):
             user_id = message.text.split(" ", 1)[1]
         except IndexError:
             return await message.reply_text("Reply to any user message or give user id, username")
+            
     try:
         user_id = int(user_id)
     except ValueError:
         pass
+        
     try:
         user = (await client.get_chat_member(message.chat.id, user_id)).user
     except:
         return await message.reply_text("Can't find you given user in this group")
+        
     try:
         await client.unban_chat_member(message.chat.id, user_id)
     except:
         return await message.reply_text(f"I don't have access to {message.command[0]} user")
+        
     await message.reply_text(f'Successfully {message.command[0]} {user.mention} from {message.chat.title}')
